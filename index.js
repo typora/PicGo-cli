d35840219c5ab130cc904c9493740fae22a93f9c
Abner Lee <ilchenearly@gmail.com>
2019-12-05 21:39:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const path = require('path')
+const minimist = require('minimist')
+let argv = minimist(process.argv.slice(2))
+let configPath = argv.c || argv.config || ''
+if (configPath !== true && configPath !== '') {
+  configPath = path.resolve(configPath)
+} else {
+  configPath = ''
+}
+const PicGo = require('picgo')
+const picgo = new PicGo(configPath)
+picgo.registerCommands()
+
+try {
+  picgo.cmd.program.parse(process.argv)
+} catch (e) {
+  picgo.log.error(e)
+  if (process.argv.includes('--debug')) {
+    Promise.reject(e)
+  }
+}
